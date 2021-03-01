c2951c912fe0e468415b5f00316fa8cba084d8cd
Plover Brown <rellwoos@gmail.com>
2021-02-28 22:14:51 -0800

    nav bar and basic page shells

---
+++ assets/js/nav.js
@@ -0,0 +1,68 @@
+const navImages = document.getElementsByClassName("navImage");
+
+console.log(navImages)
+
+document.addEventListener("mouseover", function (event) {
+    let navImg;
+    const width  = window.innerWidth || document.documentElement.clientWidth || 
+document.body.clientWidth;
+    console.log(width)
+    if (event.target.text && width >= 1200){
+        console.log()
+        switch (event.target.text.replace("(current)", "")) {
+            case "Home":
+                navImg = navImages[0]
+                break;
+            case "Musical Productions & Scripts":
+                navImg = navImages[1]
+                break;
+            case "Choreography":
+                navImg = navImages[2]
+                break;
+            case "Teaching":
+                navImg = navImages[3]
+                break;
+            case "Voiceovers":
+                navImg = navImages[4]
+                break;
+            case "Classic Laura":
+                navImg = navImages[5]
+                break;
+    
+            default:
+                break;
+        }
+        navImg.setAttribute("style", "display:block;")
+    }
+})
+
+document.addEventListener("mouseout", function (event) {
+    let navImg;
+    if (event.target.text){
+
+        switch (event.target.text.replace("(current)", "")) {
+            case "Home":
+                navImg = navImages[0]
+                break;
+            case "Musical Productions & Scripts":
+                navImg = navImages[1]
+                break;
+            case "Choreography":
+                navImg = navImages[2]
+                break;
+            case "Teaching":
+                navImg = navImages[3]
+                break;
+            case "Voiceovers":
+                navImg = navImages[4]
+                break;
+            case "Classic Laura":
+                navImg = navImages[5]
+                break;
+    
+            default:
+                break;
+        }
+        navImg.setAttribute("style", "display:none")
+    }
+})
